d6fd1aa129cf796066be18611294fdea830e2f8f
Paul Zavalza <paulz_91@hotmail.com>
2014-11-27 00:31:58 -0600

    iteration through startup dictionary of angellist

--- dbScripts/angelToLatam.py
+++ dbScripts/angelToLatam.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from urllib2 import Request, urlopen, URLError
 import json
+import httplib
 
 startups = open('startups.json')
 data = json.load(startups) 
 print data['startups']
+
+for startup in data['startups']:
+	#print startup
+	for key,value in startup.items():
+		print key, value
